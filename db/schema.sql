dbaffbdbe4258210cb9511d4c438817ab9c97872
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-07-25 20:08:56 -0700

    File base to build completed

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,4 @@
--- Drop Database if Exists Here--
--- Create Database Here --
+-- Drop Database if exists --
+DROP DATABASE IF EXISTS mvc_tech_blog_db;
+-- Create Database --
+CREATE DATABASE mvc_tech_blog_db;
--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-//Code Here
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const hbs = exphbs.create({ helpers });
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes here
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening {PORT} "));
+});
