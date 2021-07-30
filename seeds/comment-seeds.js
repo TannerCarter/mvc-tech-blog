477f430134958ea4112b5b9e1522dde245f5599d
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-07-29 23:29:09 -0700

    comment seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,18 +1,1 @@
-const { Comment } = require("../models");
-
-const commentData = [
-  {
-    user_id: 1,
-    post_id: 2,
-    comment_text: "Lorem Ipsum",
-  },
-  {
-    user_id: 2,
-    post_id: 1,
-    comment_text: "Lorem Ipsum",
-  },
-];
-
-const seedComments = () => Comment.bulkCreate(commentData);
-
-module.exports = seedComments;
+//Comment Seeds Here If Wanted
